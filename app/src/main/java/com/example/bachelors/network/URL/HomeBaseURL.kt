51428511d2412488e95813bd84a6e98405bbacf9
Arittra101 <avishek51775@gmail.com>
2025-07-08 01:23:46 +0600

    Network Layer implemented

---
+++ app/src/main/java/com/example/bachelors/network/URL/HomeBaseURL.kt
@@ -0,0 +1,5 @@
+package com.example.bachelors.network.URL
+
+object HomeBaseURL {
+   const val BASE_URL = "https://script.google.com/macros/s/AKfycbyUODnDnP4SbQvqM9YT47swQPXGLCG9DNMoFabRdk8-xzJuH5pqlji-0qq2nwYfMZ3H/exec/"
+}
